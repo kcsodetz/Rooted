b93f4c1962edf9dc978260ee2994bfe725826955
kcsodetz <kensodetz@gmail.com>
2019-08-28 19:34:34 -0400

    Add backend and mongoose

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var router = express.Router();
+
+
+mongoose.connect(process.env.MONGODB_HOST, { useNewUrlParser: true });
+mongoose.set('useCreateIndex', true);
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {

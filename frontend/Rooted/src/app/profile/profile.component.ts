2784e84e4ee35ca16d3c03124ed113639461b2d4
abigehl <aslatter@purdue.edu>
2019-09-28 18:52:24 -0400

    Basic Implementation of Profile page
    Some formatting done, haven't figured out how to pull user information yet. Unsure if editing user info is going to require a different webpage.

--- frontend/Rooted/src/app/profile/profile.component.ts
+++ frontend/Rooted/src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../services/user.service';
+import { NgForm, FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private userService: UserService, private formBuilder: FormBuilder, private _router: Router) {}
   ngOnInit() {
   }
+
 
 }

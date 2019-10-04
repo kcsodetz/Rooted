8273b06e6004046a64c2d050bf083e19f33618dd
bmalone1398 <bmalone1398@gmail.com>
2019-10-04 08:45:24 -0400

    fixed reporting groups

--- frontend/Rooted/src/app/reportgroup/reportgroup.component.ts
+++ frontend/Rooted/src/app/reportgroup/reportgroup.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {Location} from '@angular/common';
 import { NgForm, FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { TreeService } from '../services/tree.service';
-
+import { UserService} from '../services/user.service'
+import { Account } from '../models/account.model';
 @Component({
   selector: 'app-reportgroup',
   templateUrl: './reportgroup.component.html',
@@ -14,15 +15,19 @@ export class ReportgroupComponent implements OnInit {
   treeName: string;
   reporter: string;
   reportForm: FormGroup;
-  
-  constructor(private _location: Location, private treeService: TreeService, private formBuilder: FormBuilder) { }
+  username: string ="";
+  account: Account;
+  constructor(private _location: Location, private treeService: TreeService, private formBuilder: FormBuilder, private userService: UserService) { }
 
   ngOnInit() {
     console.log(history.state.data);
     this.treeID = history.state.data.treeID;
     this.treeName = history.state.data.treeName;
     this.reporter = history.state.data.reporter;
-
+    this.userService.getAccountInfo().then((res)=>{
+      this.account=new Account(res);
+      this.username=this.account.username;
+    });
     // Form inputs and validators
     this.reportForm = this.formBuilder.group({
       reason: ['']
@@ -34,8 +39,9 @@ export class ReportgroupComponent implements OnInit {
   }
 
   submitReport(treeID: string, treeName: string, reason: string, reporter: string) {
-    console.log(treeID, treeName, reason, reporter)
-    this.treeService.reportTree(treeID, treeName, reason, reporter).then(() => {
+    console.log(treeID, treeName, reason, this.username)
+    console.log(this.username)
+    this.treeService.reportTree(treeID, treeName, reason, this.username).then(() => {
       var confirm = window.alert('Tree: ' + treeName + ' Reported');
       window.location.replace("/home");
       console.log(confirm)

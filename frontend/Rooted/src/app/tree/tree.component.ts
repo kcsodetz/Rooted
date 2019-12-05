5aa696a2e7f6a524876f9f32ff5f9e761a7f9417
kcsodetz <kensodetz@gmail.com>
2019-12-05 17:50:44 -0500

    fix odd issue with ngfor

--- frontend/Rooted/src/app/tree/tree.component.ts
+++ frontend/Rooted/src/app/tree/tree.component.ts
@@ -5,19 +5,15 @@ import { Tree } from '../models/tree.model';
 import { Router, ActivatedRoute, Params, Data } from '@angular/router';
 import { UserService } from '../services/user.service';
 import { Account } from '../models/account.model';
-
-
 @Component({
   selector: 'app-tree',
   templateUrl: './tree.component.html',
   styleUrls: ['./tree.component.scss']
 })
-
 /**
  * Tree Page Component
  */
 export class TreeComponent implements OnInit {
-
   // tslint:disable-next-line: no-input-rename
   @Input('childTree') tre: Tree;
   @Output() returnToParent = new EventEmitter<string>();
@@ -45,46 +41,36 @@ export class TreeComponent implements OnInit {
   announcements: [Object];
   announcementForm: FormGroup;
   involvementForm: FormGroup;
-
   /* variables used in editing tree name*/
   renderComponent: string;
   chosenTree: Tree;
-
   constructor(private route: ActivatedRoute, public userService: UserService,
-    private treeService: TreeService,  private formBuilder: FormBuilder, private _router: Router) {
+    private treeService: TreeService, private formBuilder: FormBuilder, private _router: Router) {
     this.messages = [];
     this.treePhotoLibraryImages = [];
-
   }
-
   /**
    * Init function
    */
   ngOnInit() {
-
     /* grabs url and finds parameter 'id' */
     // var id = this.route.snapshot.params['id'];
-
     /* treeService calls getAllTreeInfo of specified ID
     * result is passed into data
     *
     * Tree model gets data and passes it to myTree
     */
-
     // this.treeService.getAllUsersInTree(id).then((data) => {
-
     // });
-
-
     // Form values and validators for create new
     this.addUserForm = this.formBuilder.group({
       username: ['', Validators.required]
     });
     this.userService.getAccountInfo().then((res) => {
       this.account = new Account(res);
-     // console.log(this.username);
+      // console.log(this.username);
       this.username = this.account.username;
-     // console.log("current username: " + this.username);
+      // console.log("current username: " + this.username);
       this.getTreeInfo();
       this.isUserAdmin();
       // this.treeMember();
@@ -94,40 +80,32 @@ export class TreeComponent implements OnInit {
       name: [''],
       sendEmail: [Boolean],
     });
-    
-
     this.displayImages();
-
     this.messageForm = this.formBuilder.group({
       message: ['']
-    })
-
+    });
     this.announcementForm = this.formBuilder.group({
       announcement: ['']
-    })
+    });
     this.announcements = [null];
     this.treeService.getAnnouncements(this.route.snapshot.params['id']).then((data) => {
       this.annObj = data;
       let x = 0;
-      while(this.annObj[x]!=undefined){
+      while (this.annObj[x] != undefined) {
         this.announcements[x] = this.annObj[x++];
       }
       console.log(this.announcements);
     });
-
     this.involvementForm = this.formBuilder.group({
       joinYear: [''],
       exitYear: ['']
-    })
-
+    });
   }
-
-   /*
-   * Get all tree information
-   */
+  /*
+  * Get all tree information
+  */
   getTreeInfo() {
     const id = this.route.snapshot.params['id'];
-
     this.treeService.getAllTreeInfo(id).then((data) => {
       this.myTree = new Tree(data);
       this.isPrivate = this.myTree.privateStatus;
@@ -140,13 +118,11 @@ export class TreeComponent implements OnInit {
         }
       }
       this.notMember = true;
-   //   console.log(data);
+      //   console.log(data);
     });
-
-
   }
   toggle(SectionName) {
-   // console.log(SectionName);
+    // console.log(SectionName);
     if (this.activeTabSection === SectionName) {
       return;
     }
@@ -160,24 +136,21 @@ export class TreeComponent implements OnInit {
    * Get all messages for a tree
    */
   getMessages() {
-
     this.treeService.getMessages(this.myTree.ID).then((messages) => {
       // this.messages = messages;
       console.log(messages);
       let i = 0;
       messages.forEach(element => {
-     //   console.log(element)
+        //   console.log(element)
         this.messages[i] = element;
         i++;
       });
       // console.log(this.messages)
     });
   }
-
   isUserAdmin() {
     // console.log("in isUserAdmin");
     const id = this.route.snapshot.params['id'];
-
     this.treeService.getAllTreeInfo(id).then((data) => {
       this.myTree = new Tree(data);
       let admins = [];
@@ -194,12 +167,10 @@ export class TreeComponent implements OnInit {
       });
     });
   }
-
   userProfile(username: string) {
     /* Navigate to /tree/id  */
     this._router.navigate(['/other-profile/' + username]);
   }
-
   /**
    * Add a message to a tree
    * @param event Event to parse input from
@@ -209,13 +180,11 @@ export class TreeComponent implements OnInit {
       this.getMessages();
     });
   }
-
   /**
    * Method that deletes a tree and redirects back to the homepage
    * @param tree Tree to be deleted
    */
   delTre(tree: Tree) {
-
     // call delete method from service
     const confirm = window.confirm('Are you sure you want to remove this tree? This action cannot be undone');
     if (confirm === false) {
@@ -229,25 +198,16 @@ export class TreeComponent implements OnInit {
       this._router.navigate(['/home']);
     });
   }
-
-
-
   // Get add user form
   get form_add_user() { return this.addUserForm.controls; }
-
   // Get form respone for toasts
   get reponse() { return this.response; }
-
-
   /**
    * Navigates to edit tree page
    */
   renderEditTreeName() {
     this._router.navigate(['/edit-name/' + this.myTree.ID]);
-
-
   }
-
   /**
    * Navigates to an admin Dashboard
    */
@@ -255,14 +215,10 @@ export class TreeComponent implements OnInit {
     /* Navigate to /tree/id  */
     this._router.navigate(['/admin/' + this.myTree.ID]);
   }
-
   setRow(_index: number) {
     this.selectedIndex = _index;
     console.log(this.selectedIndex);
   }
-
-
-
   leaveTree() {
     const username = localStorage.getItem('username');
     this.treeService.leaveTree(this.myTree.ID, username).then(() => {
@@ -273,24 +229,21 @@ export class TreeComponent implements OnInit {
       this._router.navigate(['/home']);
     });
   }
-
   /**
    * Adds user to current tree
    * @param event Event to parse user addition
    */
   addUser(event) {
-
     this.submitted = true;
     // Check if form is missing values. If true, then return.
     if (this.addUserForm.invalid) {
-   //  console.log(event);
+      //  console.log(event);
       return;
     }
-
     // Backend api call
     this.treeService.addUser(this.myTree.ID, event.value.username).then(() => {
       this.treeService.getAllTreeInfo(this.myTree.ID).then((c) => {
-     //   console.log(c);
+        //   console.log(c);
         window.location.replace('/tree/' + this.myTree.ID);
       });
     }).catch(e => {
@@ -305,21 +258,15 @@ export class TreeComponent implements OnInit {
       }
     });
   }
-
   /**
    * Get child event
    * @param event Event passed
    */
   getChildEvent(event: string) { this.returnToParent.emit('reload'); }
-
   /**
    * Naviage to home
    */
   back() { this._router.navigate(['/home']); }
-
-
-
-
   onFileChanged(event) {
     const file = event.target.files[0];
     const formdata = new FormData();
@@ -328,10 +275,9 @@ export class TreeComponent implements OnInit {
       window.location.replace('/tree/' + this.myTree.ID);
     });
   }
-
   displayImages() {
     const id = this.route.snapshot.params['id'];
-//    console.log(id);
+    //    console.log(id);
     this.treeService.getPhotos(id).then((res) => {
       let i = 0;
       res.forEach(element => {
@@ -340,108 +286,89 @@ export class TreeComponent implements OnInit {
       });
     });
   }
-
-
-  selectPhoto(string: string){
+  selectPhoto(string: string) {
     this.photoID = string;
     console.log(this.photoID);
   }
-
-  deletePhoto(){
+  deletePhoto() {
     this.treeService.deletePhoto(this.myTree.ID, this.photoID).then(() => {
       console.log('Deleting Photo');
       // navigate back to page
       window.location.replace('/tree/' + this.myTree.ID);
     });
   }
-
-
   sendJoinRequest() {
     this.treeService.requestAdminToJoinTree(this.myTree.ID, this.account.username).then((res) => {
-  //    console.log(res);
+      //    console.log(res);
       window.alert('Successfully Requested to Join Tree!');
       this.response = 'complete_editProfile';
     }).catch((error) => {
-    //  console.log(error);
+      //  console.log(error);
       window.alert('Failure! :(');
       this.response = 'fatal_error';
-
     });
-
   }
-
   async sendAddRequestUsername(form: NgForm) {
     if (this.isAdmin) {
       console.log(this.myTree.ID + ' ' + form.value.username);
       this.treeService.inviteUser(this.myTree.ID, form.value.username).then((res) => {
-              console.log(res);
-              window.alert('Success!');
-            }).catch((error) => {
-          //    console.log(error);
-              if (error.error.message === 'Bad request') {
-                window.alert('Username cannot be empty.');
-              } else {
-                window.alert(error.error.message + '.');
-              }
-              this.response = 'fatal_error';
-
-            });
-            //location.reload();
-
+        console.log(res);
+        window.alert('Success!');
+      }).catch((error) => {
+        //    console.log(error);
+        if (error.error.message === 'Bad request') {
+          window.alert('Username cannot be empty.');
+        } else {
+          window.alert(error.error.message + '.');
+        }
+        this.response = 'fatal_error';
+      });
+      // location.reload();
     } else {
       this.treeService.requestAdminToJoinTree(this.myTree.ID, form.value.username).then((res) => {
         //      console.log(res);
-              window.alert('Success!');
-              this.response = 'complete_editProfile';
-            }).catch((error) => {
-          //    console.log(error);
-              if (error.error.message === 'Bad request') {
-                window.alert('Username cannot be empty.');
-              } else {
-                window.alert(error.error.message + '.');
-              }
-              this.response = 'fatal_error';
-
-            });
-            //location.reload();
-
+        window.alert('Success!');
+        this.response = 'complete_editProfile';
+      }).catch((error) => {
+        //    console.log(error);
+        if (error.error.message === 'Bad request') {
+          window.alert('Username cannot be empty.');
+        } else {
+          window.alert(error.error.message + '.');
+        }
+        this.response = 'fatal_error';
+      });
+      // location.reload();
     }
-
   }
   async sendAddRequestEmail(form: NgForm) {
-      
   }
-
-  sendAnonMessage(message: string){
-    this.treeService.submitAnonMessage(this.myTree.ID,message).then(()=>{
-      var confirm = window.alert('Message anonymously sent to mods');
+  sendAnonMessage(message: string) {
+    this.treeService.submitAnonMessage(this.myTree.ID, message).then(() => {
+      const confirm = window.alert('Message anonymously sent to mods');
       console.log(confirm);
-    })
-    console.log("message: " + message + "submitted to mods");
-    window.location.replace('/tree/' + this.myTree.ID);
+    });
+    console.log('message: ' + message + 'submitted to mods');
   }
-
-  addAnnouncement(announcement: string){
-    this.treeService.addAnnouncement(this.route.snapshot.params['id'],announcement).then(()=> {
-      var confirm = window.alert('Announcement Requested');
+  addAnnouncement(announcement: string) {
+    this.treeService.addAnnouncement(this.route.snapshot.params['id'], announcement).then(() => {
+      const confirm = window.alert('Announcement Requested');
       console.log(confirm);
-    })
-    window.location.replace('/tree/' + this.myTree.ID);
+    });
   }
-
-  editInvolvement(joinYear: string, exitYear: string){
-    if(joinYear==''){
+  editInvolvement(joinYear: string, exitYear: string) {
+    if (joinYear == '') {
       joinYear = null;
     }
-    if(exitYear==''){
+    if (exitYear == '') {
       exitYear = null;
     }
-    console.log("joinyear " + joinYear);
-    console.log("exityear " + exitYear);
-    this.treeService.editInvolvement(this.route.snapshot.params['id'],joinYear,exitYear).then(()=>{
-      var confirm = window.alert('Your involvement has been updated');
+    console.log('joinyear ' + joinYear);
+    console.log('exityear ' + exitYear);
+    this.treeService.editInvolvement(this.route.snapshot.params['id'], joinYear, exitYear).then(() => {
+      const confirm = window.alert('Your involvement has been updated');
       console.log(confirm);
-    })
-    window.location.replace('/tree/' + this.myTree.ID);
+    });
   }
 }
+
